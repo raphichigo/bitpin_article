1af533a62b20b14d5ed347dae3afe6ad0e1b8038
sama <raphichigo@gmail.com>
2024-02-29 18:19:52 +0330

    FIX: add ArticleListItemSerializer! to serialize each item

--- article/api/controllers/article_controller.py
+++ article/api/controllers/article_controller.py
@@ -18,19 +18,22 @@ class ArticlesController(APIView):
     @swagger_auto_schema(
         operation_summary="get all articles",
         operation_id="get_all_articles",
+        query_serializer=ArticlesGetRequestSerializer,
         responses={
-            200: ...
+            200: ArticlesGetResponseSerializer,
+            400: {"detail": "bad request"}
         }
 
     )
     def get(self, request, *args, **kwargs):
         serialized_data = ArticlesGetRequestSerializer(data=request.query_params)
         if not serialized_data.is_valid():
-            return Response("bad request", status=status.HTTP_400_BAD_REQUEST)
+            return Response({"detail": "bad request"}, status=status.HTTP_400_BAD_REQUEST)
+
         page = serialized_data.validated_data.get('page')
         page_size = serialized_data.validated_data.get('page_size')
 
         articles = self.article_logic.get_article_list(page=page, page_size=page_size)
-        serialized_articles = ArticlesGetResponseSerializer(articles)
+        serialized_articles = ArticlesGetResponseSerializer({"articles": articles})
 
         return Response(serialized_articles.data, status=status.HTTP_200_OK)
--- article/api/serializers/article_serializers.py
+++ article/api/serializers/article_serializers.py
@@ -1,12 +1,16 @@
 from rest_framework import serializers
 
 
-class ArticlesGetResponseSerializer(serializers.Serializer):
+class ArticleListItemSerializer(serializers.Serializer):
     title = serializers.CharField()
     content = serializers.CharField()
     created_at = serializers.DateTimeField()
     vote_count = serializers.IntegerField()
     average_rate = serializers.FloatField()
+
+
+class ArticlesGetResponseSerializer(serializers.Serializer):
+    articles = serializers.ListSerializer(child=ArticleListItemSerializer)
 
 
 class ArticlesGetRequestSerializer(serializers.Serializer):
