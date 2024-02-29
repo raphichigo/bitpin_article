95d99b31ff46103d5e6960978e16e5dc0998a655
sama <raphichigo@gmail.com>
2024-02-29 19:15:29 +0330

    FIX: add rate api to apis

--- article/urls.py
+++ article/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from article.api.controllers.article_controller import ArticlesController
+from article.api.controllers.rating_controller import RatingController
 
 urlpatterns = [
     path('articles', ArticlesController.as_view(), name='articles'),
+    path('rate/<int:user_id>/<int:article_id>/', RatingController.as_view(), name='rate'),
 ]
